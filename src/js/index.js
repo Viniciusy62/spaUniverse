bf2317ddf4da09fb236a16bfdee884ee2c22267c
Vinicius Andrade <viniciusy62@gmail.com>
2023-09-11 17:55:21 -0300

    incluse page home.js with window.onload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+// import { Routes } from './routes.js';
+import * as Home from '../pages/home.js';
+import * as Universe from '../pages/universe.js'
+import * as Exploration from '../pages/exploration.js'
+
+
+window.onload = Home.home()
+
+// const home = document.querySelector('#home');
+// const universe = document.querySelector('#universe');
+// const exploration = document.querySelector('#exploration');
+
+// const app = document.querySelector('.app');
+
+// function route(element) {
+//   element.preventDefault()
+// }
+
+// const btn = document.querySelector('.btn');
+
+// home.addEventListener('click', route)
+// universe.addEventListener('click', route)
+// exploration.addEventListener('click', route)
+
