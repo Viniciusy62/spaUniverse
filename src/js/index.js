5b9653e354e248e17213d7ed4b0b41c47c3cdd24
Vinicius Andrade <viniciusy62@gmail.com>
2023-09-21 22:29:22 -0300

    update route function, links and routes

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,14 @@
-// import { app } from '../js/app.js';
-// import { routes } from './routes.js'
+import { app } from "./app.js";
+import { routes } from "./routes.js"
+import "./links.js"
 
-// Pages
-import * as Home from '../pages/home.js';
-import * as Universe from '../pages/universe.js'
-import * as Exploration from '../pages/exploration.js'
-
-
-
-// 
-const routes = {
-  "/": "./src/pages/home.html",
-  "/universe": "./src/pages/universe.html",
-  "/exploration": "./src/pages/exploration.html",
-  404: "./src/pages/404.html",
-}
-// 
-
-
-window.onload = Home.home()
-
-
-function route(event) {
+export function route(event) {
   event = event || window.event
   event.preventDefault()
 
   window.history.pushState({}, "", event.target.href)
   
-  // console.log(event.target.href)
-
-  // handle()
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route).then(data => data.text()).then(html => {
-    document.querySelector("#app").innerHTML = html
-  })
+  handle()
 }
 
 function handle() {
@@ -43,11 +16,10 @@ function handle() {
   const route = routes[pathname] || routes[404]
 
   fetch(route).then(data => data.text()).then(html => {
-    document.querySelector("#app").innerHTML = html
+    app.innerHTML = html
   })
 }
 
-Home.homeLink.addEventListener('click', route)
-Universe.universeLink.addEventListener('click', route)
-Exploration.explorationLink.addEventListener('click', route)
-
+home.addEventListener('click', route)
+universe.addEventListener('click', route)
+exploration.addEventListener('click', route)
---
+++ src/js/links.js
@@ -0,0 +1,3 @@
+export const home = document.querySelector("#home")
+export const universe = document.querySelector("#universe")
+export const exploration = document.querySelector("#exploration")
